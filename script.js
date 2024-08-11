ad8aa1e552447ed65cdf3c64069724ee506529fe
Stephen <ding.q.stephen@gmail.com>
2024-08-11 14:18:39 -0400

    Game works. Grid dimensions change.

--- script.js
+++ script.js
@@ -2,31 +2,43 @@ const gridContainer = document.querySelector(".grid-container");
 
 let gridSideAmount = 16;
 let gridCount =0;
-for (i =0; i<gridSideAmount; i++){
-    for(j =0; j<gridSideAmount; j++){
-    gridCount++;
-    const gridBlock = document.createElement("div");
-    gridBlock.classList.add("gridBlock");
-    const gridWidth = 1/(gridSideAmount)*100;
-    gridBlock.setAttribute("style", `width: ${gridWidth}vw; height: ${gridWidth}vh`);
-    gridContainer.appendChild(gridBlock);
-    }
-}
-console.log(gridCount);
-const allBlocks = document.querySelectorAll(".gridBlock");
-allBlocks.forEach((gridBlock) =>{
-    gridBlock.addEventListener("mouseover", () =>{
-        gridBlock.classList.add("colored");
+
+makeGrid(16);
+
+function makeGrid(gridSideAmount){
+    for (i =0; i<gridSideAmount; i++){
+        for(j =0; j<gridSideAmount; j++){
+            gridCount++;
+            const gridBlock = document.createElement("div");
+            gridBlock.classList.add("gridBlock");
+            const gridWidth = 1/(gridSideAmount)*100;
+            gridBlock.setAttribute("style", `width: ${gridWidth}vw; height: ${gridWidth}vh`);
+            gridContainer.appendChild(gridBlock);
+        }
+    }   
+
+    const allBlocks = document.querySelectorAll(".gridBlock");
+
+    allBlocks.forEach((gridBlock) =>{
+        gridBlock.addEventListener("mouseover", () =>{
+            gridBlock.classList.add("colored");
+        });
     });
-});
+
+    //console.log(gridCount);
+}
 
 const submitButton = document.querySelector(".submit");
 submitButton.addEventListener("click", ()=>{
-const inputField = document.querySelector("input");
-gridSideAmount = inputField.value;
-console.log(gridSideAmount);
-location.reload();
-
+    const inputField = document.querySelector("input");
+    gridSideAmount = inputField.value;
+    clearGrid();
+    makeGrid(gridSideAmount);
 });
 
+function clearGrid(){
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
 
