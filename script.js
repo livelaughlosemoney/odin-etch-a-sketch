9283b5eea7c33011dbd0eca140e697e8b4b20e6f
Stephen <ding.q.stephen@gmail.com>
2024-08-11 14:49:21 -0400

    Game works. Added in random color for extra credit

--- script.js
+++ script.js
@@ -21,11 +21,13 @@ function makeGrid(gridSideAmount){
 
     allBlocks.forEach((gridBlock) =>{
         gridBlock.addEventListener("mouseover", () =>{
-            gridBlock.classList.add("colored");
+            //gridBlock.classList.add("colored");
+            const newColor = generateNewColor();
+            console.log(newColor);
+            gridBlock.style.backgroundColor = newColor;
         });
     });
 
-    //console.log(gridCount);
 }
 
 const submitButton = document.querySelector(".submit");
@@ -42,3 +44,16 @@ function clearGrid(){
     }
 }
 
+const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+
+function getCharacter(index) {
+	return hexCharacters[index]
+}
+function generateNewColor() {
+	let hexColorRep = "#"
+	for (let index = 0; index < 6; index++){
+		const randomPosition = Math.floor ( Math.random() * hexCharacters.length ) 
+    	hexColorRep += getCharacter(randomPosition);
+	}
+	return hexColorRep
+}
